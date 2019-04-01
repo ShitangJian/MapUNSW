5e3149f315d55a4706a22089397886ccbb095e2f
LiYang5098332 <liyangtop1997@gmail.com>
2019-04-01 19:11:08 +1100

    UIDesign

---
+++ app/src/main/java/com/example/cris/programplaner/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.cris.programplaner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        StatusBarUtil.immersive(this);
+        findViewById(R.id.tv_register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
